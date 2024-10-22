tested model loading for LinearNet PhaseRegressor, found bug in SignPhi data set where np.sign was not included

datasets.py
@@ -20,6 +20,7 @@ class PhiDataset(Dataset):
     @staticmethod
     def sign_to_binary(x):
         # Map (-1, 1) to (0, 1) so that we can use BCELoss
+        x = np.sign(x)
         return (x + 1) / 2
     
     def open_hdf5(self):
@@ -76,10 +77,10 @@ def get_custom_dataloader(h5_file, batch_size=128, shuffle=True,
                           absPhi=False, signPhi=False):
     if not absPhi:
         dataset = PhiDataset(h5_file)
-        if signPhi:
-            dataset = SignPhiDataset(h5_file)
     else:
         dataset = AbsPhiDataset(h5_file)
+        if signPhi:
+            dataset = SignPhiDataset(h5_file)
 
     # We can use DataLoader to get batches of data
     dataloader = DataLoader(

decoder.py
@@ -143,15 +143,13 @@ class BaseDecoder(L.LightningModule):
         loss = self.loss_function(y_hat, y, x)
         
         self.log("test_loss", loss, prog_bar=True, on_epoch=True)
-        # TODO: Do we need to return y_hat, y, loss?
-        # return y_hat, y, loss
 
     def predict_step(self, batch, batch_idx, dataloader_idx=0):
         x, y = batch
         y_hat = self.model(x)
         
         # TODO: change output shapes
-        if self.model_name == "SignMLP":
+        if self.model_name == "MLP":
             print(y_hat.shape)
             y_hat = nn.Sigmoid()(y_hat)
             return y_hat.view(-1, x.size(1), x.size(2)), y.view(-1, x.size(1), x.size(2)), x
@@ -210,8 +208,8 @@ class HybridClassifier(L.LightningModule):
                                               optimizer_name,
                                               optimizer_hparams, misc_hparams,
                                               loss_hparams)
-        self.phase_regressor = self.load_regressor(model_name=model_name,
-                                                   model_id="5nozki8z")
+        self.phase_regressor = self.load_regressor(model_name="LinearNet",
+                                                   model_id="abqnogoh")
         
         # Exports the hyperparameters to a YAML file, and create "self.hparams"
         # namespace
@@ -243,6 +241,7 @@ class HybridClassifier(L.LightningModule):
             print(f"Found pretrained model at {
                   pretrained_filename}, loading...")
             # Automatically loads the model with the saved hyperparameters
+            # Loading a particular model here, so needs to be a class method?
             model = PhaseRegressor.load_from_checkpoint(
                 pretrained_filename)
 

main.py
@@ -20,7 +20,7 @@ import models
 import datasets
 
 if __name__ == '__main__':
-    np.random.seed(0x5EED+8)
+    np.random.seed(0x5EED+10)
     if len(sys.argv) == 1:
         """Run functions in this scratch area. 
         """
@@ -32,10 +32,10 @@ if __name__ == '__main__':
         train_file = './data/pretrain_numpix11_1e3_samples.h5'
         test_file = train_file
         runner = training.Trainer(train_file, test_file, test_file,
-                                  absPhi=False, signPhi=False)
+                                  absPhi=True, signPhi=True)
         runner.scan_hyperparams()
-        #runner.plot_predictions(model_name="LinearNet", model_id="6nficxdw")
-        #runner.plot_sign_predictions(model_name="SignMLP", model_id="hxu0on8b")
+        #runner.plot_predictions(model_name="LinearNet", model_id="abqnogoh")
+        #runner.plot_sign_predictions(model_name="MLP", model_id="seu67hdm")
 
 
     else:

models.py
@@ -70,7 +70,8 @@ class MLP(nn.Module):
     def forward(self, x):
         x_view = x.view(-1, x.size(1)**2)
         output = self.model(x_view)
-        return output
+        output_view = output.view(-1, x.size(1), x.size(2))
+        return output_view
     
 
 class PhaseMLP(nn.Module):